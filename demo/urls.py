19190a6917d31c73a3315a29afb6eb6528a84be0
Jacob Plotz <ja347685@ucf.edu>
2025-09-23 13:27:53 -0400

    Following part 3 of tutorial, added views that control the DVWA Docker containers and are accessible by URLs.

--- demo/urls.py
+++ demo/urls.py
@@ -4,4 +4,9 @@ from . import views
 
 urlpatterns = [
     path("", views.index, name="index"),
+    path("api/start", views.start, name="start"),
+    path("api/restart", views.restart, name="restart"),
+    path("api/stop", views.stop, name="stop"),
+    path("api/down", views.down, name="down"),
+    path("api/logs", views.logs, name="logs"),
 ]
--- demo/views.py
+++ demo/views.py
@@ -1,5 +1,40 @@
 from django.http import HttpResponse
 
+# See https://gabrieldemarmiesse.github.io/python-on-whales/
+from python_on_whales import DockerClient
+
+# Assume that DVWA is installed according to these instructions
+# https://github.com/digininja/DVWA?tab=readme-ov-file#docker
+# in a directory named DVWA which is in the parent directory of
+# this repository.
+docker = DockerClient(compose_files=["../DVWA/compose.yml"])
+
 
 def index(request):
-    return HttpResponse("Hi there")
+    return HttpResponse("Demo index page")
+
+
+def start(request):
+    docker.compose.up(detach=True)
+    return HttpResponse("Containers started")
+
+
+def restart(request):
+    docker.compose.restart()
+    return HttpResponse("Containers restarted")
+
+
+def stop(request):
+    docker.compose.stop()
+    return HttpResponse("Containers stopped")
+
+
+def down(request):
+    # see https://stackoverflow.com/a/54219039
+    # for the difference between "stop" and "down"
+    docker.compose.down()
+    return HttpResponse("Containers down")
+
+
+def logs(request):
+    return HttpResponse(docker.compose.logs())
